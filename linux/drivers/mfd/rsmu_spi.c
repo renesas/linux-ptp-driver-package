d07a7e18e8305ee8e827c1e789398b4b0bc2e541
Vincent Cheng <vincent.cheng.xh@renesas.com>
2025-09-17 14:55:33 -0400

    [STEELAI-884] Sync with idtphcdriver develop branch

--- linux/drivers/mfd/rsmu_spi.c
+++ linux/drivers/mfd/rsmu_spi.c
@@ -106,10 +106,10 @@ static int rsmu_write_page_register(struct rsmu_ddata *rsmu, u32 reg)
 			return 0;
 		page_reg = RSMU_CM_PAGE_ADDR;
 		page = reg & RSMU_PAGE_MASK;
-		buf[0] = (u8)(page & 0xff);
-		buf[1] = (u8)((page >> 8) & 0xff);
-		buf[2] = (u8)((page >> 16) & 0xff);
-		buf[3] = (u8)((page >> 24) & 0xff);
+		buf[0] = (u8)(page & 0xFF);
+		buf[1] = (u8)((page >> 8) & 0xFF);
+		buf[2] = (u8)((page >> 16) & 0xFF);
+		buf[3] = (u8)((page >> 24) & 0xFF);
 		bytes = 4;
 		break;
 	case RSMU_SABRE:
@@ -226,7 +226,7 @@ static int rsmu_spi_probe(struct spi_device *client)
 
 	rsmu->regmap = devm_regmap_init(&client->dev, NULL, client, cfg);
 	if (IS_ERR(rsmu->regmap)) {
-		ret = PTR_ERR(rsmu->regmap);
+		ret = (int)PTR_ERR(rsmu->regmap);
 		dev_err(rsmu->dev, "Failed to allocate register map: %d\n", ret);
 		return ret;
 	}
@@ -234,11 +234,13 @@ static int rsmu_spi_probe(struct spi_device *client)
 	return rsmu_core_init(rsmu);
 }
 
-static void rsmu_spi_remove(struct spi_device *client)
+static int rsmu_spi_remove(struct spi_device *client)
 {
 	struct rsmu_ddata *rsmu = spi_get_drvdata(client);
 
 	rsmu_core_exit(rsmu);
+
+	return 0;
 }
 
 static const struct spi_device_id rsmu_spi_id[] = {
@@ -262,7 +264,7 @@ MODULE_DEVICE_TABLE(of, rsmu_spi_of_match);
 static struct spi_driver rsmu_spi_driver = {
 	.driver = {
 		.name = "rsmu-spi",
-		.of_match_table = rsmu_spi_of_match,
+		.of_match_table = of_match_ptr(rsmu_spi_of_match),
 	},
 	.probe = rsmu_spi_probe,
 	.remove	= rsmu_spi_remove,
--- linux/include/linux/mfd/idtRC38xxx_reg.h
+++ linux/include/linux/mfd/idtRC38xxx_reg.h
@@ -22,6 +22,11 @@
 #define APLL_STS	(0xBD)
 #define IDET_LOCK_STS	BIT(0)
 
+#define APLL_FB_DIV_FRAC_CNFG	(0x90)
+#define APLL_FB_DIV_FRAC_MASK	GENMASK(37, 0)
+#define APLL_FB_DIV_INT_CNFG	(0x98)
+#define APLL_FB_DIV_INT_MASK	GENMASK(9, 0)
+
 /* FOD */
 #define FOD_0		(0x300)
 #define FOD_0_VFC3A	(0x400)
@@ -29,6 +34,21 @@
 #define FOD_1_VFC3A	(0x440)
 #define FOD_2		(0x380)
 #define FOD_2_VFC3A	(0x480)
+
+#define FOD_CNFG (0x0)
+#define FOD_INTEGER_MODE BIT(0)
+#define FOD_SYNC_MODE BIT(1)
+enum {
+	FOD_MODE_INTEGER	= 0,
+	FOD_MODE_SYNCHRONOUS	= 1,
+	FOD_MODE_SYNTHESIZER	= 2,
+	FOD_MODE_DCO		= 3,
+	FOD_MODE_MAX
+};
+
+#define FOD_DIV_CNFG		(0x18)
+#define FOD_DIV_FRACTION	GENMASK(39, 0)
+#define FOD_DIV_INTEGER		GENMASK(48, 40)
 
 /* TDCAPLL */
 #define TDC_CTRL		(0x44a) /* Specific to FC3W */
@@ -44,6 +64,7 @@
 #define TDC_REF_DIV_CNFG		(0x443)
 #define TDC_REF_DIV_CNFG_VFC3A		(0x163)
 #define TDC_REF_DIV_CONFIG_MASK		GENMASK(2, 0)
+#define TDC_REF_SEL			BIT(4)
 
 /* TIME SYNC CHANNEL */
 #define TIME_CLOCK_SRC		(0xa01) /* Specific to FC3W */
@@ -82,9 +103,9 @@ enum lpf_mode {
 
 #define LPF_BW_CNFG	(0xa81)
 #define LPF_BW_SHIFT	GENMASK(7, 3)
-#define LPF_BW_MULT		GENMASK(2, 0)
+#define LPF_BW_MULT	GENMASK(2, 0)
 /* 100mHz */
-#define LPF_BW_SHIFT_DEFAULT	(0x6)
+#define LPF_BW_SHIFT_DEFAULT		(0x6)
 #define LPF_BW_MULT_DEFAULT		(0x4)
 /* 20mHz */
 #define LPF_BW_SHIFT_1PPS		(0x4)
@@ -96,9 +117,9 @@ enum lpf_mode {
 #define LPF_WR_FREQ_CTRL_VFC3A	(0x730)
 
 #define TIME_CLOCK_TDC_FANOUT_CNFG	(0xB00)
-#define TIME_SYNC_TO_TDC_EN	BIT(0)
-#define SIG1_MUX_SEL_MASK	GENMASK(7, 4)
-#define SIG2_MUX_SEL_MASK	GENMASK(11, 8)
+#define TIME_SYNC_TO_TDC_EN		BIT(0)
+#define SIG1_MUX_SEL_MASK		GENMASK(7, 4)
+#define SIG2_MUX_SEL_MASK		GENMASK(11, 8)
 enum tdc_mux_sel {
 	REF0 = 0,
 	REF1 = 1,
@@ -113,7 +134,7 @@ enum tdc_mux_sel {
 };
 
 #define TIME_CLOCK_MEAS_CNFG	(0xB04)
-#define TDC_MEAS_MODE	BIT(0)
+#define TDC_MEAS_MODE		BIT(0)
 enum tdc_meas_mode {
 	CONTINUOUS = 0,
 	ONE_SHOT = 1,
@@ -121,11 +142,11 @@ enum tdc_meas_mode {
 };
 
 #define TIME_CLOCK_MEAS_DIV_CNFG	(0xB08)
-#define TIME_REF_DIV_MASK	GENMASK(29, 24)
+#define TIME_REF_DIV_MASK		GENMASK(29, 24)
 
 #define TIME_CLOCK_MEAS_CTRL	(0xB10)
-#define TDC_MEAS_EN	BIT(0)
-#define TDC_MEAS_START	BIT(1)
+#define TDC_MEAS_EN		BIT(0)
+#define TDC_MEAS_START		BIT(1)
 
 #define TDC_FIFO_READ_REQ	(0xB2F)
 #define TDC_FIFO_READ		(0xB30)
@@ -204,8 +225,22 @@ enum dpll_state {
 #define FREQMON_STS_3_VFC3A	(0x234)
 #define FREQ_FAIL_STS_SHIFT	(31)
 
+/* Clock limits */
+#define TDC_REF_FREQ_HZ			12800000
+#define XTAL_FREQ_HZ			50000000
+#define MIN_XTAL_FREQ_HZ		20000000
+#define MAX_XTAL_FREQ_HZ		150000000
+#define MAX_VCO_CLK_HZ			10750000000ULL
+#define MIN_VCO_CLK_HZ			9700000000ULL
+#define MIN_TDC_APLL_FREQ_HZ		840000000ULL
+#define MAX_TDC_APLL_FREQ_HZ		900000000ULL
+#define MIN_TDC_REF_FREQ_HZ		10000000
+#define MAX_TDC_REF_FREQ_HZ		30000000
+#define MIN_FOD_FREQ_HZ			100000000
+#define MAX_FOD_FREQ_HZ			625000000
+
 /* Firmware interface */
-#define TIME_CLK_FREQ_ADDR	(0xffa0)
+#define XTAL_FREQ_ADDR		(0xffa0)
 #define TDC_REF_FREQ_ADDR	(0xffa1)
 
 /*
@@ -222,20 +257,21 @@ enum fw_version {
 
 struct idtfc3_hw_param {
 	u32 tdc_ref_freq;
-	u32 time_clk_freq;
+	u32 xtal_freq;
 };
 
 struct idtfc3_fwrc {
-	u8 hiaddr;
-	u8 loaddr;
-	u8 value;
-	u8 reserved;
+	uint8_t hiaddr;
+	uint8_t loaddr;
+	uint8_t value;
+	uint8_t length;
+	uint8_t record[];
 } __packed;
 
 static inline void idtfc3_default_hw_param(struct idtfc3_hw_param *hw_param)
 {
-	hw_param->tdc_ref_freq = 49152000;
-	hw_param->time_clk_freq = 25000000;
+	hw_param->tdc_ref_freq = TDC_REF_FREQ_HZ;
+	hw_param->xtal_freq = XTAL_FREQ_HZ;
 }
 
 static inline int idtfc3_set_hw_param(struct idtfc3_hw_param *hw_param,
@@ -243,14 +279,14 @@ static inline int idtfc3_set_hw_param(struct idtfc3_hw_param *hw_param,
 {
 	if (addr == TDC_REF_FREQ_ADDR) {
 		/* The supported frequency range is 10MHz to 80MHz */
-		if (val > 80000000 || val < 10000000)
+		if (val > MAX_TDC_REF_FREQ_HZ || val < MIN_TDC_REF_FREQ_HZ)
 			return -EINVAL;
 		hw_param->tdc_ref_freq = val;
-	} else if (addr == TIME_CLK_FREQ_ADDR) {
-		/* Time clock period must be whole nanoseconds */
-		if (NSEC_PER_SEC % val)
+	} else if (addr == XTAL_FREQ_ADDR) {
+		/* The supported frequency range is 10MHz to 80MHz */
+		if (val > MAX_XTAL_FREQ_HZ || val < MIN_XTAL_FREQ_HZ)
 			return -EINVAL;
-		hw_param->time_clk_freq = val;
+		hw_param->xtal_freq = val;
 	} else
 		return -EFAULT;
 
